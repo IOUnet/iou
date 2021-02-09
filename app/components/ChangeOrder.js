24b2639f374c14f37ffe3ce1515ecd49eed3e96f
Stan Takt <st.takt@gmail.com>
2021-02-09 15:41:47 +0300

    changeOrder moving out

---
+++ app/components/ChangeOrder.js
@@ -0,0 +1,16 @@
+changeOrder(field, direction) {
+    this.setState({ orderByField: field, orderByDirection: direction }, () => {
+        const feedbacksS = this.state.feedbacks.sort((a,b) => {
+          if (direction == "asc") {
+            if (a[field] > b[field]) return 1;
+            if (a[field] < b[field]) return -1;
+          }
+          else if (direction == "desc") {
+            if (a[field] > b[field]) return -1;
+            if (a[field] < b[field]) return 1;
+          }
+        })
+        this.setState({ feedbacks: feedbacksS });
+    });
+}
+
