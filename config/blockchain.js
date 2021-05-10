d207ed8fcab740e5d19fc32972c357a1088906d0
Stan Takt <st.takt@gmail.com>
2021-05-10 08:33:52 +0300

    goto Celo WIP

--- config/blockchain.js
+++ config/blockchain.js
@@ -87,7 +87,31 @@ module.exports = {
       }
     ]
   },
-
+  alfajores: {
+    // fork Celo mainnet
+   
+    // do:
+    // npm install -g ganache-cli
+    
+    //  ganache-cli -f https://alfajores-forno.celo-testnet.org -m "sing bomb odor actress latin surround design squirrel engage swim illness daring office pumpkin critic room parrot pencil course gorilla thumb plate recycle hidden" -u 0x69748533FeE28A6D6ee8dB3E9E5C6dAE6fDAc99a --db ./alfajores
+    // embark run --nodashboard  alfajores
+  //  endpoint: "https://alfajores-forno.celo-testnet.org", // Endpoint of an node to connect to. Can be on localhost or on the internet
+// http://127.0.0.1:8545/
+    endpoint: "http://127.0.0.1:8545/", // Endpoint of an node to connect to. Can be on localhost or on the internet
+    accounts: [
+      {
+        //privateKeyFile: "./testnet/password",
+       // privateKey: "",// process.env.privKey,
+     //  nodeAccounts: true,
+      // numAddresses: "10",
+     // addressIndex: "1",
+        mnemonic: "sing bomb odor actress latin surround design squirrel engage swim illness daring office pumpkin critic room parrot pencil course gorilla thumb plate recycle hidden",
+        hdpath: "m/44'/60'/0'/0/",
+                   //m/44'/60'/0'/0/
+ 
+      }
+    ]
+  },
   // you can name an environment with specific settings and then specify with
   // "embark run custom_name" or "embark blockchain custom_name"
   //custom_name: {
