3f5cbff75d7dc4607523b7eb63f98c41787dfd35
stanta <st.takt@gmail.com>
2021-05-15 19:13:16 +0300

    new design  adding store

---
+++ src/ethvtx_config/createVtxStore.js
@@ -0,0 +1,50 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import { getSagas, getReducers, getInitialState, configureVtx } from 'ethvtx';
+import createSagaMiddleware from 'redux-saga';
+import thunk from 'redux-thunk'
+import indexTokenReducer from './reducers/indexTokenReducer';
+import indexTokenTokens from './reducers/getTokenTokens';
+import buyTokensReducer from './reducers/buyTokensReducer';
+import sellIndexTokenReducer from './reducers/sellTokensReducer';
+import { watchIndexTokenBuyProcess } from './sagas/buyIndexTokenSaga';
+import { watchIndexTokenSellProcess } from './sagas/sellIndexTokenSaga';
+import { watchStartBuyIndexTokens } from './sagas/getGasPrice'
+import { all } from 'redux-saga/effects'
+
+export const createVtxStore = () => {
+
+    const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+    // You can configure some parameters here to inject them with the initial state
+    const initial_state = configureVtx(getInitialState(), {
+        poll_timer: 100,
+        confirmation_treshold: 3
+    });
+
+    const additionReducers  = {
+        indexTokenReducer,
+        indexTokenTokens,
+        sellIndexTokenReducer,
+        buyTokensReducer
+    }
+
+    // Recover the vortex reducers. This method takes your custom reducers and combines them with vortex's
+    const reducers = getReducers(additionReducers);
+
+    const sagaMiddleware = createSagaMiddleware();
+
+    const store = createStore(
+        reducers,
+        initial_state,
+        composer(applyMiddleware(sagaMiddleware))
+    );
+
+    // Recover the vortex sagas. This method takes your custom sagas and combines them with vortex's
+    
+    const sagas = getSagas(store, [watchIndexTokenBuyProcess, watchIndexTokenSellProcess, watchStartBuyIndexTokens]);
+    
+    sagaMiddleware.run(sagas);
+
+    return store;
+
+};
